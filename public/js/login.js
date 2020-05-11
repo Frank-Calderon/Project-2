39429192a64f56fc23b09bcbcad0d7b38ba17c18
Frank <fcalderon3291@gmail.com>
2020-05-10 20:03:36 -0400

    fixed eslint issues and cganged passport in jason to all lower case

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,12 @@
 $(document).ready(() => {
   // Getting references to our form and inputs
-  const loginForm = $("form.login");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+  const loginForm = $('form.login');
+  const emailInput = $('input#email-input');
+  const passwordInput = $('input#password-input');
 
-<<<<<<< HEAD
-  // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", (event) => {
-=======
   // When the form is submitted, we validate there's an email and password
   // entered
   loginForm.on('submit', (event) => {
->>>>>>> d4db0e0b590ea288e57e2066c3ab0b3c1a7f40a0
     event.preventDefault();
     const userData = {
       email: emailInput.val().trim(),
@@ -22,16 +17,11 @@ $(document).ready(() => {
       return;
     }
 
-<<<<<<< HEAD
-    // If we have an email and password we run the loginUser function and clear the form
-    // eslint-disable-next-line no-use-before-define
-=======
     // If we have an email and password we run the loginUser function and clear
     // the form
->>>>>>> d4db0e0b590ea288e57e2066c3ab0b3c1a7f40a0
     loginUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
+    emailInput.val('');
+    passwordInput.val('');
   });
 
   /**
@@ -46,24 +36,13 @@ $(document).ready(() => {
  *     loginUser('example@gmail.com(opens in new tab)', secretpassword')
  */
   function loginUser(email, password) {
-    $.post("/api/login", {
+    $.post('/api/login', {
       email,
       password,
-<<<<<<< HEAD
-    })
-      .then(() => {
-        window.location.replace("/members");
-        // If there's an error, log the error
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-=======
     }).then(() => {
       window.location.replace('/members');
     }).catch((err) => {
       console.log(err);
     });
->>>>>>> d4db0e0b590ea288e57e2066c3ab0b3c1a7f40a0
   }
 });
