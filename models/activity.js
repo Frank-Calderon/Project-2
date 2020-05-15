275d0fa20a1b27b55279cdd82bc3aa70608463f1
Jeaninne <jeaninnel@hotmail.com>
2020-05-14 21:04:16 -0400

    made some changes to made Travis happy

--- models/activity.js
+++ models/activity.js
@@ -1,37 +1,37 @@
 // Creating our Activity model
 module.exports = function(sequelize, DataTypes) {
-    var Activity = sequelize.define("Activity", {
-      // The email cannot be null, and must be a proper email before creation
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      // The password cannot be null
-      lat: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-      },
-      lon: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-      },
-      checkInTime: {
-        type: DataTypes.TIME,
-        allowNull: false
-      },
-      checkOutTime: {
-        type: DataTypes.TIME,
-        allowNull: false
-      },
-      placeName: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      placeId: {
-        type: DataTypes.STRING,
-        allowNull: true
-      }
-    });
+  var Activity = sequelize.define("Activity", {
+    // The email cannot be null, and must be a proper email before creation
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    // The password cannot be null
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    lon: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    checkInTime: {
+      type: DataTypes.TIME,
+      allowNull: false
+    },
+    checkOutTime: {
+      type: DataTypes.TIME,
+      allowNull: false
+    },
+    placeName: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    placeId: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
     // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
     // Activity.prototype.checkIn = function(checkInTime) {
     //   // return bcrypt.compareSync(password, this.password);
@@ -45,5 +45,5 @@ module.exports = function(sequelize, DataTypes) {
     //   User.addHook("beforeCreate", function(user) {
     //     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     //   });
-    return Activity;
-  };
+  return Activity;
+};
